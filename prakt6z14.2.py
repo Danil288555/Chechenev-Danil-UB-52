6d28a6fdc2d3e621594b344b2d6c7e1b694172e4
Danil288555 <danamirnij1221@gmail.com>
2025-10-13 17:28:39 +0300

    prakt6

--- prakt6z14.2.py
+++ prakt6z14.2.py
@@ -7,14 +7,12 @@ for i in range(10):
 
 print("\nИсходный массив:", mas)
 
-# Вычисляем среднее арифметическое
 sum_mas = sum(mas)
 avg = sum_mas / len(mas)
 
 print(f"Сумма элементов: {sum_mas}")
 print(f"Среднее арифметическое: {avg}")
 
-# Заменяем элементы большие среднего на 1
 for i in range(len(mas)):
     if mas[i] > avg:
         mas[i] = 1
